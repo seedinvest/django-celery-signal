7aa72a68315641f5580dd3e471ba50f09d7a150b
Harshil Shah <harshil07@gmail.com>
2013-11-20 14:39:51 -0500

    enable johnny cache by default

--- django_celery_signal/tasks.py
+++ django_celery_signal/tasks.py
@@ -1,9 +1,11 @@
 from celery.task import Task
 from celery.registry import tasks
 
+from johnny.cache import enable
 
 class SignalTask(Task):
     def run(self, receiver, sender, **named):
+        enable()
         logger = self.get_logger(**named)
         logger.info("Running SignalTask for %s with args %s." % (receiver, named))
         receiver(sender=sender, **named)
